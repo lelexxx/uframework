4a16b50fd8a1f60a5d41d1fc81248a8605a8e665
Lelexxx <rruiz.alex@gmail.com>
2013-10-28 03:02:46 +0100

    Fix bug in ArticleJsonDataMapper

--- src/Mapper/ArticleJsonDataMapper.php
+++ src/Mapper/ArticleJsonDataMapper.php
@@ -6,53 +6,123 @@
 
 namespace Mapper;
 
-class ArticleJsonDataMapper implements PersistenceInterface
+use Model\Article;
+
+class ArticleJsonDataMapper implements PersistenceInterface, FinderInterface
 {
-	
-    /** Contruct
-    * 
-    */ 
-    public function __construct(){
+        /** */
+    private $datafile;
+
+    /** Collection of articles. */
+    private $articles = array();
+
+    /** Construct
+    *
+    * @param String $datafile
+    */
+    public function __construct($datafile = '/../../data/article.json'){		
+        $this->datafile = __DIR__.''.$datafile;
+
+        $array = $this->getArrayWithJson();
+
+        foreach($array->articles as $article){
+            $this->articles[$article->id] = new Article($article->id, $article->name, $article->description);
+        }
     }
 
-    /** Render an article peristante
+    /** Add a article.
     *
-    *@param $article Article
+    * @param Article $value
+    */
+    public function insert($article){
+        $this->articles[$article->getId()] = $article;
+
+        $this->saveOnJson();
+    }
+
+    /** Return all articles.
     *
     *@return array
     */
+    public function findAll($criterias = null){
+        return $this->articles;
+    }
+
+    /** Retrieve an article by its id.
+    *
+    * @param mixed $id
+    * @return null|mixed
+    */
+    public function findOneById($id){
+            return $this->articles[$id]; 
+    }
+
+    /** Get the article in array format by a json format.
+    *
+    * @return array 
+    */
+    public function getArrayWithJson(){
+            return json_decode(file_get_contents($this->datafile));
+    }
+
+    /** Get the article in JSon format.
+    *
+    * @return String 
+    */
+    public function getJson(){
+            $array = array();
+
+            foreach($this->articles as $id => $article){
+                    $array[] = $article->getName();
+            }
+
+            return json_encode($array);
+    }
+    
+    /**
+     * 
+     * @param \Mapper\Article $article
+     * @return type
+     */
     public function persist($article){
         if($article->isNew()){
-                return $this->insert($article);
+            return $this->insert($article);
         }
-
+        
         return $this->update($article);
     }
 
-    /** Delete an article from the file.
+    /** Save the articles in a file, on JSon format
     *
-    *@param $article Article
-    *
-    *@return array
     */
-    public function remove($article){
+    public function saveOnJson(){
+            $articleJson = $this->getJson();
+
+            $fp = fopen ($this->datafile, "w+");  
+            fputs($fp, $articleJson);  
+            fclose($fp);  
+    }
+    
+    /** Delete an article by its id.
+    *
+    * @param mixed $id
+    */
+    public function remove($id){		
+        unset($this->articles[$id]);
+
+        $this->saveOnJson();
     }
 
-    /** Insert an article in the file.
+    /** Update an article by its id
     *
-    *@param $article Article
-    *
-    *@return
-    */
-    public function insert($article){
-    }
-
-    /** Update an article in the file.
-    *
-    *@param $article Article
-    *
-    *@return array
+    * @param mixed $id
+    * @param mixed $value
     */
     public function update($article){
+        $articleToUpdate = $this->articles[$article->getId()];
+        $articleToUpdate->setName($article->getName());
+        $articleToUpdate->setDescription($article->getDescription());
+
+        $this->saveOnJson();
     }
 }
